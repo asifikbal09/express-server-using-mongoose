2d2557d17fb5cd1edba0bb728b4f5091c7adca87
asifikbal09 <asif.ikbal662@gmail.com>
2023-11-18 00:48:25 +0600

    Student interface, schema and model done

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,92 @@
+import { Schema, model, connect } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  Student,
+  UserName,
+} from './student/student.interface';
+
+//Create schema
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  middleName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  email: { type: String, required: true },
+  dateOfBirth: { type: String },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'block'],
+});
+
+//Create a model
+
+const Student = model<Student>('Student', studentSchema);
