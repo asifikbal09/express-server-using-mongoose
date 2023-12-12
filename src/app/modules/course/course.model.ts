9fc4b2ade9c571dd7af556aa75856e1972c83682
asifikbal09 <asif.ikbal662@gmail.com>
2023-12-12 23:31:26 +0600

    [DONE] Course interface and model complited.

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { TCourse, TPreRequisiteCourse } from './course.interface';
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourse>({
+  course: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  prefix: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  code: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const Course = model<TCourse>('Course', courseSchema);
